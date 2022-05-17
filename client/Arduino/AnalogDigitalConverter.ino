61ca8b8d98c0c1a203fb9db016b7cda4e102e624
Georg-code <jornig@outlook.com>
2022-05-17 23:07:24 +0200

    low level changes

--- client/Arduino/AnalogDigitalConverter.ino
+++ client/Arduino/AnalogDigitalConverter.ino
@@ -1,25 +1,28 @@
+/* 
+   INFO: use sudo chmod a+rw /dev/ttyUSB0 to get access to the board
+   Since the low-cost sensors are electrolytic, they only receive power
+   for a very short period of time. This slows down the erosion considerably
+*/
 
 int delayCount;
-int sensorValue;
 int sensorPin = A0;
+
 void setup()
 {
    Serial.begin(9600);
+
    pinMode(13, OUTPUT);
 }
 void loop()
 {
-// Send an average of 10 messuremnts every second. (To prevent microspikes and reduce the variation by magnetic readiation)
-   if (delayCount == 10)
+   digitalWrite(13, HIGH);
+   delay(5);
+   float sensorData = analogRead(sensorPin);
+   if (sensorData <= 0)
    {
-      Serial.println(sensorValue / delayCount);
-      sensorValue = 0;
-      delayCount = 0;
+      sensorData = 1000
    }
-   delayCount += 1;
-   sensorValue += analogRead(sensorPin);
-
-   delay(100);
+   Serial.println(1000 - sensorData);
+   digitalWrite(13, LOW);
+   delay(995);
 }
-
-
