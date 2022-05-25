a474cdff345a9e101eb365df5d10a49098890f52
Georg-code <jornig@outlook.com>
2022-05-25 08:41:07 +0200

    changed data fetching

--- serverNext/backend/pages/index.tsx
+++ serverNext/backend/pages/index.tsx
@@ -1,42 +1,42 @@
-import type { NextPage } from 'next'
-import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../styles/Home.module.css'
-import { StatsCard } from '../components/StatsCard'
-import { createStyles, Group } from '@mantine/core'
-import { Props } from 'next/script'
-import { DataChart } from '../components/DataChart'
+import type { NextPage } from "next";
+import Head from "next/head";
+import Image from "next/image";
+import styles from "../styles/Home.module.css";
+import { StatsCard } from "../components/StatsCard";
+import { createStyles, Group } from "@mantine/core";
+import { Props } from "next/script";
+import { DataChart } from "../components/DataChart";
 
 const useStyles = createStyles((theme) => ({
   StatsCard: {
-    marginTop: '20px'
-  }
+    marginTop: "20px",
+  },
 }));
 
-export const getStaticProps = async () => {
-  const res = await fetch('https://bio.niggli.software/api/v1/getdata')
-  const data = await res.json()
+export const getServerSideProps = async () => {
+  const resdata = await fetch("https://bio.niggli.software/api/v1/getdata");
+  const data = await resdata.json();
 
   return {
-    props: {waterdata: data}
-  }
-}
+    props: { waterdata: resdata },
+  };
+};
 
 const Home: NextPage = (props) => {
-  const wdata = JSON.parse(JSON.stringify(props))
-  const parseNum = (str : any) => +str.replace(/[^.\d]/g, '');
+  const wdata = JSON.parse(JSON.stringify(props));
+  const parseNum = (str: any) => +str.replace(/[^.\d]/g, "");
   const { classes } = useStyles();
 
   return (
-   <>
-   <Group position='center' className={classes.StatsCard}>
-     <StatsCard currentValue={parseNum(JSON.stringify((wdata.waterdata[0].level)))} currentTime={wdata.waterdata[0].time}></StatsCard>
+    <>
+      <Group position="center" className={classes.StatsCard}>
+        <StatsCard
+          currentValue={parseNum(JSON.stringify(wdata.waterdata[0].level))}
+          currentTime={wdata.waterdata[0].time}
+        ></StatsCard>
+      </Group>
+    </>
+  );
+};
 
-     </Group>
-     
-
-     </>
-  )
-}
-
-export default Home
+export default Home;
